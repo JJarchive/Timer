0a01a2426ef011886a6b61fb009350435307c16f
JJarchive <160006580+JJarchive@users.noreply.github.com>
2024-03-04 16:14:47 +0900

    Add files via upload

---
+++ jstest.js
@@ -0,0 +1,133 @@
+// 내 코드
+const minute = document.querySelector("#startMin");
+const second = document.querySelector("#startSec");
+const btnStart = document.querySelector(".start-btn");
+const btnPause = document.querySelector(".pause-btn");
+const btnReset = document.querySelector(".reset-btn");
+const display = document.querySelector("#display");
+const message = document.querySelector("#pause_message");
+
+let min = 0;
+let sec = 0;
+let regex = /[^0-9]/;
+let timer = null;
+let toggle = true;    // start 버튼 여러번 클릭 방지용
+
+// 0 이상의 숫자 외의 값 입력 금지
+minute.addEventListener("change", function(){
+    min = minute.value;
+    if (min < 0 | regex.test(min)){
+        alert("0 이상의 값을 입력하세요");
+    }
+})
+
+second.addEventListener("change", function(){
+    sec = second.value;
+    if(sec <= 0 | regex.test(sec)){
+        alert("0 보다 큰 값을 입력해주세요.")
+    }
+});
+
+// start 버튼
+btnStart.addEventListener("click", function(){
+    if (toggle) {                           // start 버튼 여러 번 누르는 것 방지
+        timer = setInterval(start, 1000);
+        toggle = false;
+    } else {
+        message.innerText = "이미 실행 중입니다";
+        setTimeout(function() {message.innerText = "";}, 3000);
+    }
+});
+
+// pause 버튼
+btnPause.addEventListener('click', function(){
+    clearInterval(timer);
+    btnStart.setAttribute("value", "RESUME");
+    toggle = true;
+});
+
+// reset
+btnReset.addEventListener("click", function(){
+    clearInterval(timer);
+    display.innerHTML = `0 분 0 초`;
+    reset();
+});
+
+// 함수
+function start(){
+    if (btnStart.getAttribute("value") != 'RESUME') {
+        display.innerHTML = `${min} 분 ${sec} 초`;
+        sec = sec - 1;
+        if (sec == 0 && min != 0){
+            sec = 60;
+            min -= 1;
+        }
+    } else {
+        btnStart.setAttribute("value", "START");
+    }
+    // 타임아웃 확인. 0으로 조건을 걸면 1초에서 멈춤
+    if (min == 0 && sec == -1){
+        clearInterval(timer);
+        reset();
+    }
+}
+
+function reset(){
+    message.innerHTML = "타이머가 정상 종료되었습니다.<br>2초 후에 페이지가 새로고침됩니다."
+    setTimeout(function() {location.reload();}, 3000);
+}
+
+// 강사님 코드
+var start_button = document.querySelector(".start-btn");
+start_button.addEventListener("click", startTimer);
+var stop_button = document.querySelector(".pause-btn");
+stop_button.addEventListener("click", pauseTimer);
+var reset_button = document.querySelector(".reset-btn");
+reset_button.addEventListener("click", resetTimer)
+start_flags = true;
+pause_flags = true;
+function startTimer() {
+    if (start_flags == false && pause_flags == false) { // PAUSE를 누르고 나서
+        timer = setInterval(countTimer, 1000);
+        document.querySelector('#pause_message').innerText = "타이머 다시 동작중";
+    } else if (start_flags == false && pause_flags == true) { // START 누르고 START 누르면
+        clearInterval(timer);
+        alert("이미 타이머가 동작 중입니다.")
+        timer = setInterval(countTimer, 1000);
+    } else { // 처음 시작
+        min = document.querySelector('#startMin').value;
+        sec = document.querySelector('#startSec').value;
+        timer = setInterval(countTimer, 1000);
+        start_flags = false;
+    }
+}
+function countTimer() {
+    if (sec != 0) {
+        sec = sec - 1;
+        document.querySelector('#display').innerText = min + "분 " + sec + "초";
+    }
+    else {
+        if (min != 0) {
+            min = min - 1;
+            sec = 59;
+        }
+        else {
+            document.querySelector('#display').innerText = "타이머 정상 종료";
+            document.querySelector('#pause_message').innerText = "2초 후 새로고침 됩니다.";
+            setTimeout(function () {window.location.reload()}, 2000);
+        }
+    }
+}
+function pauseTimer() {
+    clearInterval(timer);
+    pause_flags = false;
+    resume = document.querySelector(".start-btn");
+    resume.value = "RESUME";
+    document.querySelector('#pause_message').innerText = "타이머 일시 중지됨";
+}
+function resetTimer() {
+    clearInterval(timer);
+    document.querySelector('#display').innerText = "타이머 강제 리셋";
+    document.querySelector('#pause_message').innerText = "2초 후 새로고침 됩니다.";
+    setTimeout(function () {window.location.reload()}, 2000);
+}
